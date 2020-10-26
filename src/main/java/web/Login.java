0602d66f15d5defb7b6de0f2e373a89524b44927
Tobias Zimmermann <tobiaszimmermann@Tobiass-MacBook-Pro.local>
2020-10-26 14:42:40 +0100

    api 2

--- src/main/java/web/Login.java
+++ src/main/java/web/Login.java
@@ -27,7 +27,7 @@ public class Login extends Command {
 
         session.setAttribute( "user", user );
         session.setAttribute( "role", user.getRole() );
-        session.setAttribute("email", email);  // ellers skal man skrive  user.email på jsp siderne og det er sgu lidt mærkeligt at man har adgang til private felter. Men måske er det meget fedt , jeg ved det ikke
+        session.setAttribute("email", user.getEmail());  // ellers skal man skrive  user.email på jsp siderne og det er sgu lidt mærkeligt at man har adgang til private felter. Men måske er det meget fedt , jeg ved det ikke
 
 
         return user.getRole() + "page";
