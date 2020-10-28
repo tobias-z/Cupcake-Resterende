6dbe2263c4a0cda4d054d9d91613e0b05a43c438
Alexander <Alexanderhusig1@gmail.com>
2020-10-28 14:01:18 +0100

    Remove Target

--- target/classes/migrate/0.sql
+++
@@ -1,9 +0,0 @@
-use cupcake;
-
-DROP TABLE IF EXISTS properties;
-CREATE TABLE properties (
-    name VARCHAR(255) PRIMARY KEY,
-    value VARCHAR(255) NOT NULL
-);
-
-INSERT INTO properties (name, value) VALUES ("version", "0");
